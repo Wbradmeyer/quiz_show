4d0bf3ecb4660e5e7df758b75ffff0d69b5adba0
Brad Meyer <wbradmeyer@gmail.com>
2023-12-27 23:50:26 -0500

    render templates

--- gameboard_app/urls.py
+++ gameboard_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.select_board),
+    path('/', views.index),
+    path('/new', views.select_board),
+    path('/play', views.play_gameboard),
+    # path('/questions/<int:question_id>', views.display_question),
+    path('/questions/', views.display_question),
 ]
--- gameboard_app/views.py
+++ gameboard_app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return redirect('/games/new')
+
 def select_board(request):
-    return render(request, 'select_board.html')
+    return render(request, 'select_gameboard.html')
+
+def display_question(request):
+    return render(request, 'question.html')
+
+def play_gameboard(request):
+    return render(request, 'gameboard.html')
