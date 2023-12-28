1ceef27a995f7b3bb59051b1d2d1ade1810998a5
Brad Meyer <wbradmeyer@gmail.com>
2023-12-28 14:58:26 -0500

    static, clickable game, cat

--- gameboard_app/urls.py
+++ gameboard_app/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('new', views.select_board),
+    path('select', views.select_board),
     path('play', views.play_gameboard),
+    path('categories', views.display_categories),
+    # path('categories/new', views.add_category),
     # path('/questions/<int:question_id>', views.display_question),
-    path('questions/', views.display_question),
+    path('questions', views.display_question),
 ]
--- gameboard_app/views.py
+++ gameboard_app/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+# Create methods
+
+
+# Read methods
 def index(request):
-    return redirect('/games/new')
+    return redirect('/games/select')
 
 def select_board(request):
     return render(request, 'select_gameboard.html')
+
+def display_categories(request):
+    return render(request, 'categories.html')
 
 def display_question(request):
     return render(request, 'question.html')
 
 def play_gameboard(request):
     return render(request, 'gameboard.html')
+
+# Update methods
+
+
+# Delete methods
