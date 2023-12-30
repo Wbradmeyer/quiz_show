2c74095c3c1cda7a7f5127378c9c8dcfeb54a5b9
Brad Meyer <wbradmeyer@gmail.com>
2023-12-29 22:15:38 -0500

    foreign keys

--- gameboard_app/models.py
+++ gameboard_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    game_assigned = models.ForeignKey(Game, related_name="categories_avail", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Question(models.Model):
+    question = models.TextField()
+    answer = models.CharField(max_length=255)
+    points = models.IntegerField(default=100)
+    category_assigned = models.ForeignKey(Category, related_name="question_list", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
