d6f28d88c9c162856a4d37caad13347598eb72ef
Brad Meyer <wbradmeyer@gmail.com>
2024-01-15 18:12:23 -0500

    render login page

--- quiz_show/urls.py
+++ quiz_show/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('user_app.urls')),
+    path('games/', include('gameboard_app.urls')),
     path('admin/', admin.site.urls),
-    path('/', include('user_app.urls')),
-    path('games/', include('gameboard_app.urls'))
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return redirect('/')
+
+def login(request):
+    return  redirect('/')
