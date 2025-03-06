139e1d8f03c7ebb08e259df88fb0587b26cf2efb
W. Brad Meyer <wbradmeyer@gmail.com>
2025-03-06 11:17:12 -0500

    setup ques errors on form

--- gameboard_app/views.py
+++ gameboard_app/views.py
@@ -27,11 +27,12 @@ def create_category(request):
 def create_question(request):
     # add validations here
     errors = Question.objects.question_validator(request.POST)
-    category = Category.get_by_id(request.POST['cat_id'])
     if len(errors) > 0:
+        cat_id = request.POST['cat_id']
         for key, value in errors.items():
             messages.error(request, value)
-        return redirect(f'/games/categories/{category.id}')
+        return redirect(f'/games/categories/{cat_id}')
+    category = Category.get_by_id(request.POST['cat_id'])
     file_form = UploadFileForm(request.POST, request.FILES)
     Question.add_question({'question': request.POST['question'], 
                         'answer': request.POST['answer'],
